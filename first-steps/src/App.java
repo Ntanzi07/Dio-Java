35ad9bd44faf9d7b557060dbdca09d74d44926f1
Ntanzi07 <nathan.tanzi12345@gmail.com>
2024-09-02 16:51:46 -0300

    Operadores!

--- first-steps/src/App.java
+++ first-steps/src/App.java
@@ -1,5 +1,27 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class App{
+    public static void main(String[] args){
+        //Tipos de variaveis:
+        String name = "Nathan";
+        int age = 2;
+        double weight = 70.8;
+        char gender = 'M';
+        boolean likeCoffee = true;
+        //Date dateOfBirth = new Date();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("weight: " + weight);
+        System.out.println("gender: " + gender);
+
+        System.out.print("Like coffee? ");
+        
+        String resultado = likeCoffee ? "yes!!" : "No!!";
+        System.out.print(resultado);
+
+        System.out.println("\n");
+
+
+    
+
     }
 }
