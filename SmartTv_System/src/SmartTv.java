f2a5a14425f508244debeac950b3abdb67a63b57
Ntanzi07 <nathan.tanzi12345@gmail.com>
2024-09-04 16:09:07 -0300

    Smart Tv

--- SmartTv_System/src/SmartTv.java
+++ SmartTv_System/src/SmartTv.java
@@ -3,11 +3,40 @@ public class SmartTv {
     int channel = 1;
     int volume = 25;
 
+    //on/off
     public void on(){
         isOn = true;
     }
     public void off(){
         isOn = false;
     }
-    
+
+    //Volume
+    public void volumeUp(){
+        volume++;
+        System.out.println("Volume: " + volume);
+    }
+    public void volumeDown(){
+        volume--;
+        System.out.println("Volume: " + volume);
+    }
+    public void newVolume(int newValue){
+        volume = newValue;
+        System.out.println("Volume: " + volume);
+    }
+
+    //Channel
+    public void channelUp(){
+        channel++;
+        System.out.println("Channel: " + channel);
+    }
+    public void channelDonw(){
+        channel--;
+        System.out.println("Channel: " + channel);
+    }
+    public void newChannel(int newValue){
+        channel = newValue;
+        System.out.println("Channel: "+ channel);
+    }
+
 }
--- SmartTv_System/src/User.java
+++ SmartTv_System/src/User.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class User {
     public static void main(String[] args) throws Exception {
         SmartTv smarTv = new SmartTv();
+
+        Scanner entrada = new Scanner(System.in);
 
         System.out.println("Tv is on?: " + smarTv.isOn);
         System.out.println("your channel is: "+ smarTv.channel);
@@ -8,5 +12,22 @@ public class User {
 
         smarTv.on();
         System.out.println("Tv is on?: " + smarTv.isOn);
+
+        smarTv.volumeUp();
+        smarTv.volumeUp();
+        smarTv.volumeUp();
+        smarTv.volumeDown();
+
+        System.out.println("new volume:");
+        smarTv.newVolume(entrada.nextInt());
+
+        smarTv.channelUp();
+        smarTv.channelUp();
+        smarTv.channelDonw();
+        System.out.println("new channel:");
+        smarTv.newChannel(entrada.nextInt());
+
+        
+        entrada.close();
     }
 }
