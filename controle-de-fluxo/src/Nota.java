237850508f04ca5ec5d0c86d9851e74b4d39625f
Ntanzi07 <nathan.tanzi12345@gmail.com>
2024-09-09 15:02:10 -0300

    Nota

---
+++ controle-de-fluxo/src/Nota.java
@@ -0,0 +1,9 @@
+public class Nota {
+    public static void main(String[] args) throws Exception {
+
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+    }
+}
