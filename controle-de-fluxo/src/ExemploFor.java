ebc50c9120b4bbb5983a8355f49a7f89c880e5ba
Ntanzi07 <nathan.tanzi12345@gmail.com>
2024-09-09 20:13:46 -0300

    For and Arrays

--- controle-de-fluxo/src/ExemploFor.java
+++ controle-de-fluxo/src/ExemploFor.java
@@ -1,7 +1,14 @@
 public class ExemploFor {
     public static void main(String[] args) {
-        for( int i = 0; i <= 10; i++ ){
+        //for
+        for( int i = 1; i <= 5; i++ ){
             System.out.println(i);
+        }
+        
+        //for em Arrays
+        String nomes[] = {"Nathan", "Enrico", "Marco", "Guilherme", "Rafa", "Lyniker"};
+        for (int i = 0;i <= nomes.length; i++){
+            System.out.println(nomes[i]);
         }
     }
 }
