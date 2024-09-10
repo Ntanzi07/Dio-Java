4631a05b686f68ec1cc98b0b96b5c43faa65ab5c
Ntanzi07 <nathan.tanzi12345@gmail.com>
2024-09-10 14:36:02 -0300

    while

---
+++ controle-de-fluxo/src/While.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class While {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+         while (mesada>0) {
+            double valorDoce = valorAleatorio();
+            if (valorDoce > mesada) 
+                valorDoce = mesada;
+            
+            System.out.println("\nDoce no valor de: " + valorDoce + " adicionado ao carrinho!");
+            mesada -= valorDoce;
+            System.out.println("Mesada = " + mesada);
+         }
+         
+    }
+
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+
+}
