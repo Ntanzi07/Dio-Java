37d9bf8053b0a696f1262afd19674bee6f58b751
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2024-12-16 16:20:43 -0300

    adicionando mais exercicios

--- DIO-OOP/Interfaces/equipamentos/multifuncional/EquipamentoMultiFuncioal.java
+++ DIO-OOP/Interfaces/equipamentos/multifuncional/EquipamentoMultiFuncioal.java
@@ -1,5 +1,22 @@
-package multifuncional;
-public class EquipamentoMultiFuncioal {
+package equipamentos.multifuncional;
 
-     
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultiFuncioal implements Copiadora, Digitalizadora, Impressora {
+    @Override
+    public void copiar() {
+        System.out.println("Copiando!");
+        
+    }
+    @Override
+    public void digitalizar(){
+        System.out.println("Digitalizando!");
+    }
+    @Override
+    public void imprimir(){
+        System.out.println("imprimindo!");
+    }
+
 }
