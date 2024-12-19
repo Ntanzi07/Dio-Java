b055f5f354243164fc13dd5a989e91912e8ca819
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2024-12-19 12:58:10 -0300

    Atualizando ListaEncadeada: adicionando getNo() e validIndex()

--- Estrutura-de-Dados/ListaEncadeada/ListaEncadeada.java
+++ Estrutura-de-Dados/ListaEncadeada/ListaEncadeada.java
@@ -1,7 +1,5 @@
 package ListaEncadeada;
 
-import java.security.PublicKey;
-
 public class ListaEncadeada<T> {
     
     private No<T> refNoEntrada;
@@ -28,6 +26,22 @@ public class ListaEncadeada<T> {
         }
     }
 
+    public No<T> getNo(int index){
+        No<T> refAuxiliar = refNoEntrada;
+        No<T> noRetorno = null;
+
+        this.validaIndex(index);
+        
+        for(int i = 0; i <= this.size() - 1; i++){
+            if(i == index){
+                noRetorno = refAuxiliar;
+                break;
+            }
+            refAuxiliar = refAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
     public int size(){
         No<T> refComplementar = refNoEntrada;
         int tamanhoDaLista = 0;
@@ -50,10 +64,16 @@ public class ListaEncadeada<T> {
     @Override
     public String toString() {
         String str = 
-            "--------------------------\n"
-            + "     ListaEncadeada\n"
+              "--------------------------\n"
+            + "      ListaEncadeada\n"
             + "--------------------------\n";
         str += refNoEntrada.toStringEncadeado();
         return str;
+    }
+
+    private void validaIndex(int index){
+        int tam = this.size() - 1;
+        if(index > tam)
+            throw new IndexOutOfBoundsException("Não existe conteuo no indece " + "desta lista. O tamanho atual e de " + tam + '.');
     }
 }
